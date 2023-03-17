91ef8546889cefb24af1632ecf0257b73d1706ee
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-03-17 11:38:47 +0000

    profileHeader added

---
+++ dfxtra-frontend/src/components/profileHeader/profileHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const profileHeader = ({ user }) => {
+    return (
+        <div className="profileHeader">
+            <img src={user.profilePicture} alt="Profile picture not found" className="profilePicture" />
+            <h1>{user.displayName}</h1>
+            <h2>{user.tagLine}</h2>
+            <div className="Overview"> {user.overview} </div>
+            <div className="Links">Links
+                <a href="https://github.com" target="_blank" rel="noopener noreferrer" >Github</a>
+                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" >LinkedIn</a>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default profileHeader;
