1d2dd6545465efd1514d166df379218e013a8339
Elle Ordona <eto0507@hotmail.co.uk>
2023-03-21 11:04:38 +0000

    added experience css

--- dfxtra-frontend/src/components/Experience/ExperienceItem/ExperienceItems.jsx
+++ dfxtra-frontend/src/components/Experience/ExperienceItem/ExperienceItems.jsx
@@ -1,11 +1,18 @@
 // Individual Experience Component
 
-const ExperienceItem = ({experience}) => {
-    const { employerName, employerImage, position, description, startDate, endDate, reference } = experience
+const ExperienceItem = ({ experience }) => {
+	const { employerName, employerImage, position, description, startDate, endDate, reference } = experience;
 	return (
-		<div>
-			<h4>{employerName}</h4>
-			<img src={employerImage} alt="logo of the employer" />
+		<div className="ex-item">
+			<div className="ex-employer">
+				<img src={employerImage} alt="logo of the employer" />
+				<h4 className="ex-name">{employerName}</h4>
+			</div>
+			<p className="ex-position">{position}</p>
+			<p className="ex-dates">
+				{startDate} - {endDate}
+			</p>
+			<button>Expand</button>
 		</div>
 	);
 };
