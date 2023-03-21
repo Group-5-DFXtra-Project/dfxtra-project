9696203a0af72a94b038f06e1e91199b1c323d21
Sharif Khan <52325540+sharifgk@users.noreply.github.com>
2023-03-21 13:09:36 +0000

    Update App.js

--- dfxtra-frontend/src/App.js
+++ dfxtra-frontend/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import data from "./sampleProfileInfo.json"
 import Certifications from './components/Certifications/Certifications.jsx'
 import Experience from './components/Experience/Experience.jsx'
+import Header from './components/Header/Header.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import ProfileHeader from './components/profileHeader/profileHeader.jsx'
 import Qualifications from './components/Qualifications/Qualifications.jsx'
 
 const sampleProfile = JSON.parse(JSON.stringify(data))
@@ -9,15 +12,20 @@ const sampleProfile = JSON.parse(JSON.stringify(data))
 const certificationsData = sampleProfile.profileInfo.certifications;
 const qualificationsData = sampleProfile.profileInfo.qualifications;
 const experienceData = sampleProfile.profileInfo.experience;
+const displayName = sampleProfile.profileInfo.profileHeader.displayName;
+const user = sampleProfile.profileInfo.profileHeader;
 
 
 
 function App() {
   return (
     <div className="App">
-      <Certifications certificationsData={certificationsData}/>
-      <Qualifications qualificationsData={qualificationsData}/>
-      <Experience experienceData={experienceData}/>
+      <Navbar />
+      <Header displayName={displayName} />
+      <ProfileHeader user={user} />
+      <Certifications certificationsData={certificationsData} />
+      <Qualifications qualificationsData={qualificationsData} />
+      <Experience experienceData={experienceData} />
 
     </div>
   );
