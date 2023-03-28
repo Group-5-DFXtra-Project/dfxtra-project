1a8a25c3c4115828c450c5a5623ea31018188bf6
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-03-28 14:36:44 +0100

    y

--- dfxtra-frontend/src/components/Certifications/CertificationForm.jsx
+++ dfxtra-frontend/src/components/Certifications/CertificationForm.jsx
@@ -1,32 +1,33 @@
 import { useState } from "react"
-import {submitCert} from "../../utils/DataServices.js"
+import { submitCert } from "../../utils/DataServices.js"
 
-function CertificationForm({setIsCertificationForm}){
+function CertificationForm({ setIsCertificationForm }) {
 
-const [certificationFormInput, setCertificationFormInput] = useState("")
+    const [certificationFormInput, setCertificationFormInput] = useState("")
 
-const handleSubmit = async (e) =>{
-    e.preventDefault()
-    try{
-    const response = await submitCert(certificationFormInput)
-    setIsCertificationForm(false)
-    return response 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await submitCert(certificationFormInput)
+            console.log(response);
+            setIsCertificationForm(false)
+            return response
+        }
+
+        catch (e) {
+            return { error: e.code, errorMessage: e.message }
+        }
+
     }
-    
-    catch(e){
-        return {error: e.code, errorMessage: e.message}
+
+    const handleChange = (e) => {
+        setCertificationFormInput(e.target.value)
     }
 
-}
-
-const handleChange = (e) =>{
-    setCertificationFormInput(e.target.value)
-}
-
-    return(
+    return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="Certification">Certification:</label>
-            <input type="text" name="CertificationName" value= {certificationFormInput} onChange={handleChange}/>
+            <input type="text" name="CertificationName" value={certificationFormInput} onChange={handleChange} />
             <button type="submit">Submit</button>
         </form>
     )
