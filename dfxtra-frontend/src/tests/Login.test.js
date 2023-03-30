ff9f2496fb938a280be74c1e616aa5e690f22d1c
Elle Ordona <eto0507@hotmail.co.uk>
2023-03-30 15:17:07 +0100

    login tests failing

---
+++ dfxtra-frontend/src/tests/Login.test.js
@@ -0,0 +1,36 @@
+// Testing for Login in Component
+
+// imports
+import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Login from '../components/Login/Login.jsx';
+
+jest.mock('../utils/authServices.js');
+
+describe('Testing Login Component', () => {
+	xtest('should call submitHandler when log in button is clicked', () => {
+		const getProfileMock = jest.fn(() => {});
+		const logInSubmitHandlerMock = jest.fn();
+
+		render(
+			<BrowserRouter>
+				<Login getProfileInfo={getProfileMock} />
+			</BrowserRouter>
+		);
+		const input1 = screen.getByPlaceholderText('Enter your email');
+		const input2 = screen.getByPlaceholderText('Enter a password');
+		const submitButton = screen.getByTitle('login-button');
+
+		act(() => {
+			userEvent.type(input1, 'j@j');
+			userEvent.type(input2, 'j');
+		});
+
+		act(() => {
+			userEvent.click(submitButton);
+		});
+
+		expect(logInSubmitHandlerMock).toHaveBeenCalledTimes(1);
+	});
+});
